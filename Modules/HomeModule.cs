6357d6064688960c311801486ed0ceff95fc7640
John Dowd <irishdowd10@gmail.com>
2017-06-09 13:04:30 -0700

    add index, clients, stylists along wiht homemodule

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace HairSalon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["index.cshtml", AllStylists];
+      };
+      Get["/clients"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["clients.cshtml", AllClients];
+      };
+      Get["/stylists"] = _ => {
+      List<Stylist> AllStylists = Stylist.GetAll();
+      return View["stylists.cshtml", AllStylists];
+      };
+    }
+  }
+}
